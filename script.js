4255360d6c165ab780de6a608161d9a95a1d5ff2
Lakshya Kumar <lakshyakumar5023@gmail.com>
2024-05-10 09:49:15 +0530

    completed (responsiveness left)

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+function locomotiveScroll(){
+    gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
 });
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, {duration: 0, disableLerp: true}) : locoScroll.scroll.instance.scroll.y;
+  }, 
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.defaults({ scroller: "#main" });
+
+
+ScrollTrigger.refresh();
+
+}
+
+locomotiveScroll();
+
+const scroll = new LocomotiveScroll({
+      el: document.querySelector('#main'),
+      smooth: true
+})
